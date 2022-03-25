c78335d43debe1df1a97ea28b4dceca764afd845
kvarel <kvarel2509@gmail.com>
2022-03-26 00:13:48 +0300

    привязана postgres

--- comearth/settings.py
+++ comearth/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'comearth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'comearth_db',
+        'USER': 'postgres',
+        'PASSWORD': 'HrkL3825Hz',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
